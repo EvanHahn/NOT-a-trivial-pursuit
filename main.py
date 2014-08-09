1f9ce51b7ace608723e8dc719b63bcbded809a1d
Evan Hahn <me@evanhahn.com>
2014-08-09 11:22:03 -1000

    don't remove duplicate players; creates issues

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from steps.trash_existing_tables import trash_existing_tables
 from steps.create_tables import create_tables
 from steps.populate_tables import populate_tables
-from steps.remove_duplicate_players import remove_duplicate_players
 
 if __name__ == '__main__':
     trash_existing_tables()
     create_tables()
     populate_tables()
-    remove_duplicate_players()
